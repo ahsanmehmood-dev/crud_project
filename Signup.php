4feed77572d959d6c470c71ece8911fea56dae20
ahsanmehmood-dev <ahsanmehmood2002@gmail.com>
2023-06-06 19:20:46 +0500

    UI created

---
+++ Signup.php
@@ -0,0 +1,86 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+</head>
+<style>
+    img {
+        height: 734px;
+        width: 550px;
+        border-radius: 1rem 0 0 1rem;
+    }
+
+    .card {
+        border-radius: 1rem;
+
+    }
+</style>
+
+<body>
+    <section class="">
+        <div class="container py-3">
+            <div class="row d-flex justify-content-center align-items-center">
+                <div>
+                    <div class="card">
+                        <div class="row">
+                            <div class="col-md-6 col-lg-5 d-none d-md-block">
+                                <img src="https://images.unsplash.com/photo-1528716321680-815a8cdb8cbe?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=700&q=90"
+                                    alt="img" />
+                            </div>
+                            <div class="col-md-6 d-flex align-items-center">
+                                <div class="card-body p-4 p-lg-5 text-black">
+
+                                    <form>
+                                        <h3 class="mb-3 pb-3">Sign into Your Account</h3>
+
+                                        <div class="form-outline mb-4">
+                                            <input type="text" id="form2Example17"
+                                                class="form-control form-control-lg" />
+                                            <label class="form-label" for="form2Example17">Name</label>
+                                        </div>
+
+                                        <div class="form-outline mb-4">
+                                            <input type="email" id="form2Example17"
+                                                class="form-control form-control-lg" />
+                                            <label class="form-label" for="form2Example17">Email address</label>
+                                        </div>
+
+                                        <div class="form-outline mb-4">
+                                            <input type="password" id="form2Example27"
+                                                class="form-control form-control-lg" />
+                                            <label class="form-label" for="form2Example27">Password</label>
+                                        </div>
+
+                                        <div class="form-outline mb-4">
+                                            <input type="password" id="form2Example27"
+                                                class="form-control form-control-lg" />
+                                            <label class="form-label" for="form2Example27">Confirm Password</label>
+                                        </div>
+
+                                        <div class="pt-1 mb-4">
+                                            <button class="btn btn-dark btn-lg" type="submit">Register</button>
+                                        </div>
+
+                                        <p class="mb-5 pb-lg-2">Already have an account? <a
+                                                href="Login.php">Register here</a></p>
+                                    </form>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz"
+    crossorigin="anonymous"></script>
+
+</html>
